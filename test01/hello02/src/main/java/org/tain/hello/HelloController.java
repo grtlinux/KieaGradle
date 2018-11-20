59eea88ea60b2541fe9d76d360cd05a2ccb5fe4c
grtlinux <grtlinux@gmail.com>
2018-11-21 03:31:25 +0900

    create 1st

---
+++ test01/hello02/src/main/java/org/tain/hello/HelloController.java
@@ -0,0 +1,26 @@
+package org.tain.hello;
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class HelloController {
+
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String hello(Locale locale, Model model) {
+		Date date = new Date();
+		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+		
+		String formattedDate = dateFormat.format(date);
+		
+		model.addAttribute("serverTime", formattedDate);
+		
+		return "index";
+	}
+}
