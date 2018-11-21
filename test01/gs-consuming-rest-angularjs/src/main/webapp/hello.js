802fd572738266c2631538b6ca662808be3c4ad4
grtlinux <grtlinux@gmail.com>
2018-11-21 14:46:09 +0900

    create 1st

---
+++ test01/gs-consuming-rest-angularjs/src/main/webapp/hello.js
@@ -0,0 +1,9 @@
+angular.module('demo', [])
+.controller('Hello', function($scope, $http) {
+    $http.get('http://rest-service.guides.spring.io/greeting').
+        then(function(response) {
+            $scope.greeting = response.data;
+        });
+});
+
+
