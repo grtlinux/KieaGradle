d2fed4a8e7760e8ddf8b9a3dd9b83a49f84eceb4
grtlinux <grtlinux@gmail.com>
2018-11-28 17:56:01 +0900

    modify

--- test01/gs-accessing-data-mysql/src/main/java/sql/query.sql
+++ test01/gs-accessing-data-mysql/src/main/java/sql/query.sql
@@ -5,10 +5,10 @@ USE jpadb;
 DROP TABLE IF EXISTS user;
 
 CREATE TABLE user (
-  id      int(20)                            NOT NULL,
+  id      int(20)         AUTO_INCREMENT     NOT NULL,
   name    varchar(255)    CHARACTER SET utf8 NOT NULL,
   email   VARCHAR (255)   CHARACTER SET utf8 NOT NULL,
   PRIMARY KEY (id)
-) default charset = utf8;
+) AUTO_INCREMENT=1 default charset=utf8;
 
 
